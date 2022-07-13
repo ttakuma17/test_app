38ac6573ec99eb119283dd7f8146a3a44641a510
ttakuma17 <ttanimiya@gmail.com>
2022-07-13 22:41:33 +0900

    add categories page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'homes/qa'
   get '/' => 'homes#top'
   get '/qa' => 'homes#qa'
+  get '/categories' => 'categories#index'
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
